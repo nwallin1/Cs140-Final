20db70cc03d35a97d0b054b85a7a47380c8d19f7
Benjamin Buchanan <33266490+Nynergy@users.noreply.github.com>
2018-05-03 08:45:17 -0400

    Add files via upload
    Here are the java files for what I have so far. What I'm missing is the FullAssembler and whatever makeReady() needs to be. I'll just ask in lab today. I wouldn't try importing these files into your workspace, because the buildpaths will be different. Just open them in Notepad and copy/paste whatever you need that way.

---
+++ projectview/Animator.java
@@ -0,0 +1,36 @@
+package projectview;
+
+import javax.swing.Timer;
+
+public class Animator {
+
+	private static final int TICK = 500;
+	private boolean autoStepOn = false;
+	private Timer timer;
+	private ViewMediator view;
+	
+	public Animator(ViewMediator view) {
+		this.view = view;
+	}
+	
+	void toggleAutoStep() {
+		autoStepOn = !autoStepOn;
+	}
+	
+	void setPeriod(int period) {
+		timer.setDelay(period);
+	}
+	
+	public void start() {
+		timer = new Timer(TICK, e -> {if(autoStepOn) view.step();});
+		timer.start();
+	}
+
+	public boolean isAutoStepOn() {
+		return autoStepOn;
+	}
+
+	public void setAutoStepOn(boolean autoStepOn) {
+		this.autoStepOn = autoStepOn;
+	}
+}
